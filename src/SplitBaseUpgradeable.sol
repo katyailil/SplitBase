1041043a91cb0b93b756dcc0244073af3d8f0302
Katya Ili <katyyailinets@gmail.com>
2025-11-11 08:40:57 +0100

    fix(upgrade): rebase SplitBaseUpgradeable with proper OwnableUpgradeable init

--- src/SplitBaseUpgradeable.sol
+++ src/SplitBaseUpgradeable.sol
@@ -1,26 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
 contract SplitBaseUpgradeable is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal _version;
 
-    constructor() {
-        _disableInitializers();
-    }
-
-    function initialize() public initializer {
-        __Ownable_init(msg.sender);
+    function initialize(address initialOwner) public initializer {
+        __Ownable_init(initialOwner);
         __UUPSUpgradeable_init();
         _version = 1;
     }
 
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
-
     function getVersion() external view returns (uint256) {
         return _version;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+
+    // helper for next versions
+    function setVersion(uint256 v) external onlyOwner {
+        _version = v;
     }
 }
