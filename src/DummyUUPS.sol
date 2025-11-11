2379816fc0eea4d1e155d0891e88a32f59512f53
Katya Ili <katyyailinets@gmail.com>
2025-11-11 08:37:51 +0100

    feat: add DummyUUPS minimal implementation for upgrade path diagnostics

---
+++ src/DummyUUPS.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract DummyUUPS is UUPSUpgradeable, OwnableUpgradeable {
+    function initialize() public initializer {
+        __Ownable_init(msg.sender);
+        __UUPSUpgradeable_init();
+    }
+
+    function _authorizeUpgrade(address) internal override onlyOwner {}
+}
