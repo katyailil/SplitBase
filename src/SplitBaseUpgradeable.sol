88d6fed0cb4495cad937e3eddaf2eee5450bc57a
Katya Ili <katyyailinets@gmail.com>
2025-11-11 12:47:15 +0100

    refactor(core): SplitBaseUpgradeable to use AdminUUPS; keep initialize(address) ABI

--- src/SplitBaseUpgradeable.sol
+++ src/SplitBaseUpgradeable.sol
@@ -1,16 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "./base/AdminUUPS.sol";
 
-contract SplitBaseUpgradeable is Initializable, OwnableUpgradeable, UUPSUpgradeable {
+contract SplitBaseUpgradeable is AdminUUPS {
     uint256 internal _version;
 
-    function initialize(address initialOwner) public initializer {
-        __Ownable_init(initialOwner);
-        __UUPSUpgradeable_init();
+    function initialize(address admin_) public initializer {
+        __AdminUUPS_init(admin_);
         _version = 1;
     }
 
@@ -18,10 +15,7 @@ contract SplitBaseUpgradeable is Initializable, OwnableUpgradeable, UUPSUpgradea
         return _version;
     }
 
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
-
-    // helper for next versions
-    function setVersion(uint256 v) external onlyOwner {
+    function setVersion(uint256 v) external onlyAdmin {
         _version = v;
     }
 }
