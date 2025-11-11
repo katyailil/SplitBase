a78035fdc7c160ae4685256151eecece796150db
Katya Ili <katyyailinets@gmail.com>
2025-11-11 08:09:18 +0100

    feat(scripts): upgrade UUPS proxy to new SplitBaseUpgradeable implementation

--- script/UpgradeUUPS.s.sol
+++ script/UpgradeUUPS.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 import "forge-std/Script.sol";
-import {SplitBaseV1} from "../src/implementations/SplitBaseV1.sol";
+import {SplitBaseUpgradeable} from "../src/SplitBaseUpgradeable.sol";
 
 interface IUUPS {
     function upgradeTo(address newImplementation) external;
@@ -12,7 +12,7 @@ contract UpgradeUUPS is Script {
     function run() external {
         address proxy = vm.envAddress("PROXY");
         vm.startBroadcast();
-        SplitBaseV1 impl = new SplitBaseV1();
+        SplitBaseUpgradeable impl = new SplitBaseUpgradeable();
         IUUPS(proxy).upgradeTo(address(impl));
         vm.stopBroadcast();
         console2.log("Implementation", address(impl));
